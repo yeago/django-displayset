637d3cb904d307e12d3b38df4de3fbf78aacfcca
Sam <samueltoriel@gmail.com>
2010-02-17 21:58:12 +0000

    Updated admin actions so that leaving the selected blank means selecting all

--- views.py
+++ views.py
@@ -4,12 +4,13 @@ from django.core.exceptions import PermissionDenied
 from django.contrib.admin.views.main import ChangeList
 from django.contrib.admin import options as adminoptions
 from django.core.paginator import Paginator, InvalidPage
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect,HttpResponse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.utils.http import urlencode
 from django.db.models import Q
 from django.db import models
+from django.contrib.admin import helpers
 
 class DefaultDisplaySite(object):
 	actions = []
@@ -284,4 +285,69 @@ class DisplaySet(adminoptions.ModelAdmin):
 			'admin/change_list.html'
 		], context, context_instance=context_instance)
 	
+	def response_action(self, request, queryset):
+		"""
+		Handle an admin action. This is called if a request is POSTed to the
+		changelist; it returns an HttpResponse if the action was handled, and
+		None otherwise.
+		"""
+		# There can be multiple action forms on the page (at the top
+		# and bottom of the change list, for example). Get the action
+		# whose button was pushed.
+		try:
+			action_index = int(request.POST.get('index', 0))
+		except ValueError:
+			action_index = 0
 
+		# Construct the action form.
+		data = request.POST.copy()
+		data.pop(helpers.ACTION_CHECKBOX_NAME, None)
+		data.pop("index", None)
+
+		# Use the action whose button was pushed
+		try:
+			data.update({'action': data.getlist('action')[action_index]})
+		except IndexError:
+			# If we didn't get an action from the chosen form that's invalid
+			# POST data, so by deleting action it'll fail the validation check
+			# below. So no need to do anything here
+			pass
+
+		action_form = self.action_form(data, auto_id=None)
+		action_form.fields['action'].choices = self.get_action_choices(request)
+
+		# If the form's valid we can handle the action.
+		if action_form.is_valid():
+			action = action_form.cleaned_data['action']
+			select_across = action_form.cleaned_data.get('select_across',None)
+			func, name, description = self.get_actions(request)[action]
+
+			# Get the list of selected PKs. If nothing's selected, we can't
+			# perform an action on it, so bail. Except we want to perform
+			# the action explicitely on all objects.
+
+			### We change the default action of returning none as if we want to try returning all <<<<
+			"""
+			selected = request.POST.getlist(helpers.ACTION_CHECKBOX_NAME)
+			if not selected and not select_across:
+				# Reminder that something needs to be selected or nothing will happen
+				msg = _("Items must be selected in order to perform actions on them. No items have been changed.")
+				self.message_user(request, msg)
+				return None
+
+			if not select_across:
+				# Perform the action only on the selected objects
+				queryset = queryset.filter(pk__in=selected)
+			"""
+			response = func(self, request, queryset)
+
+			# Actions may return an HttpResponse, which will be used as the
+			# response from the POST. If not, we'll be a good little HTTP
+			# citizen and redirect back to the changelist page.
+			if isinstance(response, HttpResponse):
+				return response
+			else:
+				return HttpResponseRedirect(".")
+		else:
+			msg = _("No action selected.")
+			self.message_user(request, msg)
