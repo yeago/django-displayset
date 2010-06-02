50d02e239c018b8271332934caa08344960c7d3d
Stephen <subsume@gmail.com>
2010-06-02 15:27:18 +0000

    paired down

--- views.py
+++ views.py
@@ -39,8 +39,7 @@ ORDER_VAR = 'o'
 ORDER_TYPE_VAR = 'ot'
 MAX_SHOW_ALL = 1000
 class DisplayList(ChangeList):
-	def __init__(self,queryset,request,*args,**kwargs):
-		self.filtered_queryset = queryset
+	def __init__(self,request,*args,**kwargs):
 		super(DisplayList,self).__init__(request,*args,**kwargs)
 		self.multiple_params_safe = dict(request.GET.lists()) #<<<<
 	
@@ -71,41 +70,9 @@ class DisplayList(ChangeList):
 		#<<<<
 		return '?%s' % urlencode(final_params)
 
-	def get_results(self, request):
-		paginator = Paginator(self.query_set, self.list_per_page)
-		# Get the number of objects, with admin filters applied.
-		result_count = paginator.count
-
-		# Get the total number of objects, with no admin filters applied.
-		# Perform a slight optimization: Check to see whether any filters were
-		# given. If not, use paginator.hits to calculate the number of objects,
-		# because we've already done paginator.hits and the value is cached.
-		if not self.query_set.query.where:
-			full_result_count = result_count
-		else:
-			full_result_count = self.root_query_set.count()
-
-		can_show_all = MAX_SHOW_ALL #<<<<
-		multi_page = result_count > self.list_per_page
-
-		# Get the list of objects to display on this page.
-		if (self.show_all and can_show_all) or not multi_page:
-			result_list = self.query_set._clone()
-		else:
-			try:
-				result_list = paginator.page(self.page_num+1).object_list
-			except InvalidPage:
-				result_list = ()
-
-		self.result_count = result_count
-		self.full_result_count = full_result_count
-		self.result_list = result_list
-		self.can_show_all = can_show_all
-		self.multi_page = multi_page
-		self.paginator = paginator
-
-
+	"""
 	def get_ordering(self):
+		print super(DisplayList,self).get_ordering()
 		lookup_opts, params = self.lookup_opts, self.params
 		# For ordering, first check the "ordering" parameter in the admin
 		# options, then check the object's default ordering. If neither of
@@ -143,6 +110,7 @@ class DisplayList(ChangeList):
 		if ORDER_TYPE_VAR in params and params[ORDER_TYPE_VAR] in ('asc', 'desc'):
 			order_type = params[ORDER_TYPE_VAR]
 		return order_field, order_type
+	"""
 
 	def get_query_set(self):
 		# Set ordering.
@@ -180,6 +148,9 @@ class DisplaySet(adminoptions.ModelAdmin):
 	auto_redirect_url = None
 	export = False
 	export_name = None
+	def get_changelist(self,request):
+		DisplayList.filtered_queryset = self.filtered_queryset
+		return DisplayList
 	#<<<<
 
 	#<<<<
@@ -244,129 +215,6 @@ class DisplaySet(adminoptions.ModelAdmin):
 	
 	def queryset(self, request):
 		return self.filtered_queryset
-	
-	def changelist_view(self, request, extra_context=None):
-		"The 'change list' admin view for this model."
-		from django.contrib.admin.views.main import ERROR_FLAG
-		from django.contrib.admin.options import IncorrectLookupParameters
-		opts = self.model._meta
-		app_label = opts.app_label
-		
-		# Check actions to see if any are available on this changelist
-		actions = self.get_actions(request)
-
-		# Remove action checkboxes if there aren't any actions available.
-		list_display = list(self.list_display)
-		if not actions:
-			try:
-				list_display.remove('action_checkbox')
-			except ValueError:
-				pass
-
-		try:
-			cl = DisplayList(self.filtered_queryset,request, self.model, list_display, self.list_display_links, self.list_filter,
-		self.date_hierarchy, self.search_fields, self.list_select_related, self.list_per_page, self.list_editable, self)
-		except IncorrectLookupParameters:
-			# Wacky lookup parameters were given, so redirect to the main
-			# changelist page, without parameters, and pass an 'invalid=1'
-			# parameter via the query string. If wacky parameters were given and
-			# the 'invalid=1' parameter was already in the query string, something
-			# is screwed up with the database, so display an error page.
-			if ERROR_FLAG in request.GET.keys():
-				return render_to_response('admin/invalid_setup.html', {'title': 'Database error'})
-			return HttpResponseRedirect(request.path + '?' + ERROR_FLAG + '=1')
-
-		#<<<<
-		# if auto_redirect is true we should handle that before anything else
-		if self.auto_redirect and cl.query_set.count() == 1:
-			obj = cl.query_set[0]
-			try:
-				url = obj.get_absolute_url()
-			except AttributeError:
-				url = None
-			if url: # if no url just go ahead and show the display set normally
-				return HttpResponseRedirect(url)
-		#<<<<
-
-		# If the request was POSTed, this might be a bulk action or a bulk edit.
-		# Try to look up an action first, but if this isn't an action the POST
-		# will fall through to the bulk edit check, below.
-		if actions and request.method == 'POST':
-			response = self.response_action(request, queryset=cl.get_query_set())
-			if response:
-				return response
-
-		# If we're allowing changelist editing, we need to construct a formset
-		# for the changelist given all the fields to be edited. Then we'll
-		# use the formset to validate/process POSTed data.
-		formset = cl.formset = None
-
-		# Handle POSTed bulk-edit data.
-		if request.method == "POST" and self.list_editable:
-			FormSet = self.get_changelist_formset(request)
-			formset = cl.formset = FormSet(request.POST, request.FILES, queryset=cl.result_list)
-			if formset.is_valid():
-				changecount = 0
-				for form in formset.forms:
-					if form.has_changed():
-						obj = self.save_form(request, form, change=True)
-						self.save_model(request, obj, form, change=True)
-						form.save_m2m()
-						change_msg = self.construct_change_message(request, form, None)
-						self.log_change(request, obj, change_msg)
-						changecount += 1
-
-				if changecount:
-					if changecount == 1:
-						name = force_unicode(opts.verbose_name)
-					else:
-						name = force_unicode(opts.verbose_name_plural)
-					msg = ungettext("%(count)s %(name)s was changed successfully.",
-									"%(count)s %(name)s were changed successfully.",
-									changecount) % {'count': changecount,
-													'name': name,
-													'obj': force_unicode(obj)}
-					self.message_user(request, msg)
-
-				return HttpResponseRedirect(request.get_full_path())
-
-		# Handle GET -- construct a formset for display.
-		elif self.list_editable:
-			FormSet = self.get_changelist_formset(request)
-			formset = cl.formset = FormSet(queryset=cl.result_list)
-
-		# Build the list of media to be used by the formset.
-		if formset:
-			media = self.media + formset.media
-		else:
-			media = self.media
-
-		# Build the action form and populate it with available actions.
-		if actions:
-			action_form = self.action_form(auto_id=None)
-			action_form.fields['action'].choices = self.get_action_choices(request)
-		else:
-			action_form = None
-
-		context = {
-			'title': cl.title,
-			'is_popup': cl.is_popup,
-			'cl': cl,
-			'media': media,
-			'has_add_permission': self.has_add_permission(request),
-			'root_path': self.admin_site.root_path,
-			'app_label': app_label,
-			'action_form': action_form,
-			'actions_on_top': self.actions_on_top,
-			'actions_on_bottom': self.actions_on_bottom,
-		}
-		context.update(extra_context or {})
-		context_instance = RequestContext(request, current_app=self.admin_site.name)
-		return render_to_response(self.change_list_template or [
-			'admin/%s/%s/change_list.html' % (app_label, opts.object_name.lower()),
-			'admin/%s/change_list.html' % app_label,
-			'admin/change_list.html'
-		], context, context_instance=context_instance)
 	
 	def response_action(self, request, queryset):
 		"""
