ff4604ff1a06220b3d1837ac05efa65451368b33
Sam <samueltoriel@gmail.com>
2010-06-04 15:59:13 +0000

    moved most of the extra functionality from the modeladmin to the changelist

--- views.py
+++ views.py
@@ -9,9 +9,14 @@ from django.http import HttpResponseRedirect,HttpResponse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.utils.http import urlencode
+from django.utils.translation import ungettext
+from django.utils.encoding import force_unicode
 from django.db.models import Q
 from django.db import models
 from django.contrib.admin import helpers
+from django import template
+
+from django_displayset import forms as dsforms
 
 class DefaultDisplaySite(object):
 	actions = []
@@ -27,7 +32,8 @@ class DefaultDisplaySite(object):
 		return update_wrapper(no_wrap, view)
 
 def generic(request,queryset,display_class,extra_context=None,display_site=DefaultDisplaySite):
-	return display_class(queryset,display_site).changelist_view(request,extra_context)
+	display = display_class(queryset,display_site)
+	return display.changelist_view(request,extra_context)
 
 def list_replace(replacements, original): # replacements are [(index,function),(index,function),...]
 	for item in replacements: # item is (index, function)
@@ -35,13 +41,66 @@ def list_replace(replacements, original): # replacements are [(index,function),(
 		original.insert(item[0],item[1])
 	return original
 
+
+# How to add csv export to your own display set
+# class DisplaySetSubclass(DisplaySet):
+#	export = True
+#	export_name = "display_report" ####makes the file name display_report.csv
+def csv_export(modeladmin, request, queryset):
+	import re
+	html_re = re.compile("<.*>(.*)</.*>")
+	response = HttpResponse(mimetype='text/csv')
+	response['Content-Disposition'] = 'attachment; filename=%s.csv' % (modeladmin.export_name or queryset.model)
+	writer = csv.writer(response)
+	fields = []
+	header = []
+	for f in modeladmin.list_display:
+		if f != 'action_checkbox':
+			if callable(f):
+				fields.append(f)
+				try:
+					header.append(f.short_description)
+				except AttributeError:
+					header.append(f.__name__)
+				continue
+			fields.append(f);header.append(f)
+	writer.writerow(header)
+	for obj in queryset:
+		row = []
+		for f in fields:
+			if f != 'action_checkbox':
+				if callable(f):
+					text = f(obj)
+					try:
+						text = html_re.search(text).groups()[0]
+						row.append(text)
+					except (TypeError,AttributeError): # either we got something like a datetime or no match was found (no html, so its clean)
+						row.append(text)
+					continue
+				row.append(getattr(obj, f))
+		writer.writerow(row)
+	return response
+csv_export.short_description = "Export to Excel"
+#<<<<
+
 ORDER_VAR = 'o'
 ORDER_TYPE_VAR = 'ot'
 MAX_SHOW_ALL = 1000
 class DisplayList(ChangeList):
+
+	#<<<<
 	def __init__(self,request,*args,**kwargs):
 		super(DisplayList,self).__init__(request,*args,**kwargs)
-		self.multiple_params_safe = dict(request.GET.lists()) #<<<<
+		self.multiple_params_safe = dict(request.GET.lists())
+		self.model_admin.default_list_display = self.handle_default_display() 
+		self.list_display = self.handle_list_display()
+		if not self.model_admin.actions:
+			try:
+				self.list_display.remove('action_checkbox')
+			except ValueError:
+				pass
+		
+	#<<<<
 	
 	def get_query_string(self, new_params=None, remove=None):
 		if new_params is None: new_params = {}
@@ -70,48 +129,6 @@ class DisplayList(ChangeList):
 		#<<<<
 		return '?%s' % urlencode(final_params)
 
-	"""
-	def get_ordering(self):
-		print super(DisplayList,self).get_ordering()
-		lookup_opts, params = self.lookup_opts, self.params
-		# For ordering, first check the "ordering" parameter in the admin
-		# options, then check the object's default ordering. If neither of
-		# those exist, order descending by ID by default. Finally, look for
-		# manually-specified ordering from the query string.
-		ordering = self.model_admin.ordering or lookup_opts.ordering or ['-' + lookup_opts.pk.name]
-
-		if ordering[0].startswith('-'):
-			order_field, order_type = ordering[0][1:], 'desc'
-		else:
-			order_field, order_type = ordering[0], 'asc'
-		if ORDER_VAR in params:
-			try:
-				field_name = self.list_display[int(params[ORDER_VAR])]
-				try:
-					f = lookup_opts.get_field(field_name)
-				except models.FieldDoesNotExist:
-					# See whether field_name is a name of a non-field
-					# that allows sorting.
-					try:
-						if callable(field_name):
-							attr = field_name
-						elif hasattr(self.model_admin, field_name):
-							attr = getattr(self.model_admin, field_name)
-						else:
-							attr = getattr(self.model, field_name)
-						order_field = attr.admin_order_field
-					except AttributeError:
-						if field_name in self.filtered_queryset.query.aggregates or field_name in self.filtered_queryset.query.extra: #<<<<
-							order_field = field_name
-				else:
-					order_field = f.name
-			except (IndexError, ValueError):
-				pass # Invalid ordering specified. Just use the default.
-		if ORDER_TYPE_VAR in params and params[ORDER_TYPE_VAR] in ('asc', 'desc'):
-			order_type = params[ORDER_TYPE_VAR]
-		return order_field, order_type
-	"""
-
 	def get_query_set(self):
 		# Set ordering.
 		if self.order_field:
@@ -139,51 +156,28 @@ class DisplayList(ChangeList):
 
 		return self.filtered_queryset
 
-class DisplaySet(adminoptions.ModelAdmin):
-	#<<<<
-	change_list_template = 'displayset/base.html'
-	use_get_absolute_url = [] 
-	default_list_display = [] 
-	auto_redirect = False
-	auto_redirect_url = None
-	export = False
-	export_name = None
-	def get_changelist(self,request):
-		DisplayList.filtered_queryset = self.filtered_queryset
-		return DisplayList
-	#<<<<
-
-	#<<<<
-	def __init__(self,queryset,display_set_site,*args,**kwargs):
-		self.filtered_queryset = queryset
-		super(DisplaySet,self).__init__(queryset.model,display_set_site)
-		self.default_list_display = self.handle_default_display() 
-		self.list_display = self.handle_list_display()
-		self.add_action_export()		
-	#<<<<
-		
 	#<<<<
 	def handle_default_display(self):
 		replace_list = []
-		for x,f in enumerate(self.default_list_display):
+		for x,f in enumerate(self.model_admin.default_list_display):
 			func = self.get_absolute_urlify(f)
 			if func:
 				replace_list.append((x,func))
-		return list_replace(replace_list,self.default_list_display)
+		return list_replace(replace_list,self.model_admin.default_list_display)
 
 	def handle_list_display(self):
 		replace_list = []
-		for x,f in enumerate(self.list_display):
+		for x,f in enumerate(self.model_admin.list_display):
 			func = self.get_absolute_urlify(f)
 			if func:
 				replace_list.append((x,func))
 
-		self.list_display = self.prepend_default_display()
-		return list_replace(replace_list,self.list_display)
+		self.model_admin.list_display = self.prepend_default_display()
+		return list_replace(replace_list,self.model_admin.list_display)
 	
 	def prepend_default_display(self):
-		list_display = self.list_display[:]
-		for f in reversed(self.default_list_display):
+		list_display = self.model_admin.list_display[:]
+		for f in reversed(self.model_admin.default_list_display):
 			if list_display[0] == 'action_checkbox': 
 				list_display.insert(1,f) # action checkbox is in the first slot
 			else: list_display.insert(0,f)
@@ -191,11 +185,11 @@ class DisplaySet(adminoptions.ModelAdmin):
 
 	def get_absolute_urlify(self,field):
 		func = None
-		if field in self.use_get_absolute_url:
+		if field in self.model_admin.use_get_absolute_url:
 			func = lambda obj: "<a href='%s'>%s</a>" % (obj.get_absolute_url(), getattr(obj,func.field)) # or func.field
 			func.admin_order_field = field
 			func.short_description = field
-		elif callable(field) and field.__name__ in self.use_get_absolute_url:
+		elif callable(field) and field.__name__ in self.model_admin.use_get_absolute_url:
 			func = lambda obj: "<a href='%s'>%s</a>" % (obj.get_absolute_url, func.field(obj)) # or func.field(obj)
 			try:
 				func.admin_order_field = field.admin_order_field
@@ -211,11 +205,31 @@ class DisplaySet(adminoptions.ModelAdmin):
 			func.field = field
 			return func
 		return None
+
+class DisplaySet(adminoptions.ModelAdmin):
 	#<<<<
-	
+	change_list_template = 'displayset/base.html'
+	use_get_absolute_url = [] 
+	default_list_display = [] 
+	auto_redirect = False
+	auto_redirect_url = None
+	export = False
+	export_name = None
+
+	def __init__(self,queryset,display_set_site,*args,**kwargs):
+		self.filtered_queryset = queryset
+		if self.export:
+			self.actions.append(csv_export)
+		super(DisplaySet,self).__init__(queryset.model,display_set_site)
+
+	def get_changelist(self,request):
+		DisplayList.filtered_queryset = self.filtered_queryset
+		return DisplayList
+		
 	def queryset(self, request):
 		return self.filtered_queryset
-	
+	#<<<<
+
 	def response_action(self, request, queryset):
 		"""
 		Handle an admin action. This is called if a request is POSTed to the
@@ -284,50 +298,138 @@ class DisplaySet(adminoptions.ModelAdmin):
 			msg = "No action selected."
 			self.message_user(request, msg)
 
-	#<<<<
-	# How to add csv export to your own display set
-	# class DisplaySetSubclass(DisplaySet):
-	#	export = True
-	#	export_name = "display_report" ####makes the file name display_report.csv
+	def changelist_view(self, request, extra_context=None):
+		"The 'change list' admin view for this model."
+		from django.contrib.admin.views.main import ERROR_FLAG
+		opts = self.model._meta
+		app_label = opts.app_label
+		if not self.has_change_permission(request, None):
+			raise PermissionDenied
 
-	def add_action_export(self):
-		if self.export == True:
-			self.actions.append(self.csv_export)
+		# Check actions to see if any are available on this changelist
+		actions = self.get_actions(request)
 
-	def csv_export(self, modeladmin, request, queryset):
-		import re
-		html_re = re.compile("<.*>(.*)</.*>")
-		response = HttpResponse(mimetype='text/csv')
-		response['Content-Disposition'] = 'attachment; filename=%s.csv' % (self.export_name or queryset.model)
-		writer = csv.writer(response)
-		fields = []
-		header = []
-		for f in modeladmin.list_display:
-			if f != 'action_checkbox':
-				if callable(f):
-					fields.append(f)
-					try:
-						header.append(f.short_description)
-					except AttributeError:
-						header.append(f.__name__)
-					continue
-				fields.append(f);header.append(f)
-		writer.writerow(header)
-		for obj in queryset:
-			row = []
-			for f in fields:
-				if f != 'action_checkbox':
-					if callable(f):
-						text = f(obj)
-						try:
-							text = html_re.search(text).groups()[0]
-							row.append(text)
-						except (TypeError,AttributeError): # either we got something like a datetime or no match was found (no html, so its clean)
-							row.append(text)
-						continue
-					row.append(getattr(obj, f))
-			writer.writerow(row)
-		return response
-	csv_export.short_description = "Export to Excel"
-	#<<<<
+		# Remove action checkboxes if there aren't any actions available.
+		list_display = list(self.list_display)
+		if not actions:
+			try:
+				list_display.remove('action_checkbox')
+			except ValueError:
+				pass
+
+		ChangeList = self.get_changelist(request)
+		try:
+			cl = ChangeList(request, self.model, list_display, self.list_display_links, self.list_filter,
+				self.date_hierarchy, self.search_fields, self.list_select_related, self.list_per_page, self.list_editable, self)
+		except adminoptions.IncorrectLookupParameters:
+			# Wacky lookup parameters were given, so redirect to the main
+			# changelist page, without parameters, and pass an 'invalid=1'
+			# parameter via the query string. If wacky parameters were given
+			# and the 'invalid=1' parameter was already in the query string,
+			# something is screwed up with the database, so display an error
+			# page.
+			if ERROR_FLAG in request.GET.keys():
+				return render_to_response('admin/invalid_setup.html', {'title': ('Database error')})
+			return HttpResponseRedirect(request.path + '?' + ERROR_FLAG + '=1')
+		#<<<<
+		# if auto_redirect is true we should handle that before anything else
+		if self.auto_redirect and cl.query_set.count() == 1:
+
+			obj = cl.query_set[0]
+			try:
+				url = obj.get_absolute_url()
+			except AttributeError:
+				url = None
+
+			if url: # if no url just go ahead and show the display set normally
+				return HttpResponseRedirect(url)
+		#<<<<
+
+		# If the request was POSTed, this might be a bulk action or a bulk
+		# edit. Try to look up an action or confirmation first, but if this
+		# isn't an action the POST will fall through to the bulk edit check,
+		# below.
+		action_failed = False
+		selected = request.POST.getlist(helpers.ACTION_CHECKBOX_NAME)
+
+		# Actions with no confirmation
+		if actions and request.method == 'POST':
+			response = self.response_action(request, queryset=cl.get_query_set())
+			if response:
+				return response
+
+		# If we're allowing changelist editing, we need to construct a formset
+		# for the changelist given all the fields to be edited. Then we'll
+		# use the formset to validate/process POSTed data.
+		formset = cl.formset = None
+
+		# Handle POSTed bulk-edit data.
+		if (request.method == "POST" and self.list_editable and
+				'_save' in request.POST and not action_failed):
+			FormSet = self.get_changelist_formset(request)
+			formset = cl.formset = FormSet(request.POST, request.FILES, queryset=cl.result_list)
+			if formset.is_valid():
+				changecount = 0
+				for form in formset.forms:
+					if form.has_changed():
+						obj = self.save_form(request, form, change=True)
+						self.save_model(request, obj, form, change=True)
+						form.save_m2m()
+						change_msg = self.construct_change_message(request, form, None)
+						self.log_change(request, obj, change_msg)
+						changecount += 1
+
+				if changecount:
+					if changecount == 1:
+						name = force_unicode(opts.verbose_name)
+					else:
+						name = force_unicode(opts.verbose_name_plural)
+					msg = ungettext("%(count)s %(name)s was changed successfully.",
+									"%(count)s %(name)s were changed successfully.",
+									changecount) % {'count': changecount,
+													'name': name,
+													'obj': force_unicode(obj)}
+					self.message_user(request, msg)
+
+				return HttpResponseRedirect(request.get_full_path())
+
+		# Handle GET -- construct a formset for display.
+		elif self.list_editable:
+			FormSet = self.get_changelist_formset(request)
+			formset = cl.formset = FormSet(queryset=cl.result_list)
+
+		# Build the list of media to be used by the formset.
+		if formset:
+			media = self.media + formset.media
+		else:
+			media = self.media
+
+		# Build the action form and populate it with available actions.
+		if actions:
+			action_form = self.action_form(auto_id=None)
+			action_form.fields['action'].choices = self.get_action_choices(request)
+		else:
+			action_form = None
+
+		context = {
+			'module_name': force_unicode(opts.verbose_name_plural),
+			'title': cl.title,
+			'is_popup': cl.is_popup,
+			'cl': cl,
+			'media': media,
+			'has_add_permission': self.has_add_permission(request),
+			'root_path': self.admin_site.root_path,
+			'app_label': app_label,
+			'action_form': action_form,
+			'actions_on_top': self.actions_on_top,
+			'actions_on_bottom': self.actions_on_bottom,
+			'actions_selection_counter': self.actions_selection_counter,
+		}
+		context.update(extra_context or {})
+		context_instance = template.RequestContext(request, current_app=self.admin_site.name)
+		return render_to_response(self.change_list_template or [
+			'admin/%s/%s/change_list.html' % (app_label, opts.object_name.lower()),
+			'admin/%s/change_list.html' % app_label,
+			'admin/change_list.html'
+		], context, context_instance=context_instance)
 
