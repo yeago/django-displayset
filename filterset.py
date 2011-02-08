4f3ebe1b2c932c73f2c605b25ceb3abc828c1252
Stephen <subsume@gmail.com>
2011-02-08 21:29:12 +0000

    x

--- filterset.py
+++ filterset.py
@@ -4,10 +4,12 @@ class ParameterFilterSet(django_filters.FilterSet):
 	def get_parameters(self):
 		parameters = []
 		skip_list = ['submit', 'q', 'o', 'ot', 'p']
+
 		if 'submit' in self.data:
 			for h,k in self.data.lists():
 				if self.data[h] and not h.lower() in skip_list:
 					parameters.append((h,k))
+
 		parameters = sorted(parameters)
 		return parameters
 
